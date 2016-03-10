cbb2c9a3881ea56646a37b01920c58af2dfef4fe
N-O-M-A-D <thomasbwood@hotmail.com>
2016-03-10 18:05:23 +0100

    Display 8 words, up to 3 of which are correct

--- app/src/main/java/com/example/tom/myvocabbuilder/MainActivity.java
+++ app/src/main/java/com/example/tom/myvocabbuilder/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.tom.myvocabbuilder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     int wordNumber = 0;
     int i;
     int j;
+    int noOfEnglishWords = 0;
 
     List<List<List>> indexGermanToEnglishJava = new ArrayList<List<List>>();
     List<String> englishWords = new ArrayList<String>();
@@ -84,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
         List<String> deWord = new ArrayList<String>();
         List<String> enWords = new ArrayList<String>();
+        List<Integer> indexNoEnWords = new ArrayList<Integer>();
         /**
          * The ~ symbol delineates the word and number(s) in each entry
          */
@@ -94,14 +98,29 @@ public class MainActivity extends AppCompatActivity {
          */
         for (i=1; i < entrySize; i++) {
             List<String> entry = new ArrayList<String>(Arrays.asList(entries.get(i).split("~")));
+
             enWords.add(new String(entry.get(0)));
+            englishWords.add(new String(entry.get(0)));
+
         }
+        Log.w("MainActivity.java", "i= " + i);
+        /**
+         * Note that the entry includes the German word, so the size of the entry is one more than
+         * the number of English words
+         */
+        indexNoEnWords.add(noOfEnglishWords);
+        noOfEnglishWords = noOfEnglishWords + i - 1;
+        indexNoEnWords.add(noOfEnglishWords);
+
+        Log.w("MainActivity.java", "Number of English Words = " + noOfEnglishWords);
+        Log.w("MainActivity.java", "Number of English Words = " + englishWords.size());
 
         /**
          * The De word list has only one entry
          */
         arrayWords.add(new ArrayList(deWord));
         arrayWords.add(new ArrayList(enWords));
+        arrayWords.add(new ArrayList(indexNoEnWords));
 
         return arrayWords;
     }
@@ -119,19 +138,38 @@ public class MainActivity extends AppCompatActivity {
         TextView germanWord = (TextView) findViewById(R.id.german_word);
         germanWord.setText(String.valueOf(indexGermanToEnglishJava.get(number).get(0).get(0)));
 
-        int entrySize = indexGermanToEnglishJava.get(number).get(1).size();
-        Log.w("MainActivity.java", "THE Translation ENTRY SIZE IS " + entrySize);
-        int random = (int) (Math.random() * entrySize);
-        Log.w("MainActivity.java", "THE chosen entry IS " + random);
+        int entrySizeEn = indexGermanToEnglishJava.get(number).get(1).size();
+        Log.w("MainActivity.java", "THE Translation ENTRY SIZE IS " + entrySizeEn);
+        int noOfTranslationsToShow = Math.min(entrySizeEn, 3);
 
-        List<String> choices = new ArrayList<String>();
+        List<Integer> indexNoEnWords = indexGermanToEnglishJava.get(number).get(2);
 
-        choices.add(new String(String.valueOf(indexGermanToEnglishJava.get(number).get(1).get(random))));
+        Integer entryStartEnWords = indexNoEnWords.get(0);
 
-        choices.add(new String(String.valueOf(indexGermanToEnglishJava.get(number).get(1).get(random))));
+        Set<String> choices = new HashSet<String>();
 
+        while (choices.size() < noOfTranslationsToShow) {
 
-        displayTranslations(choices);
+            int random = (int) (Math.random() * entrySizeEn);
+            Log.w("MainActivity.java", "THE chosen entry IS " + random);
+
+            choices.add(new String(String.valueOf(indexGermanToEnglishJava.get(number).get(1).get(random))));
+
+        }
+
+        while (choices.size() < 8) {
+            int random = (int) (Math.random() * (englishWords.size() - entrySizeEn));
+            Log.w("MainActivity.java", "THE chosen entry number IS " + random);
+            if(random >= entryStartEnWords)
+                random = random + entrySizeEn;
+            Log.w("MainActivity.java", "THE chosen entry IS " + random);
+
+            choices.add(new String(String.valueOf(englishWords.get(random))));
+        }
+
+        List<String> choicesArray = new ArrayList<String>(choices);
+
+        displayTranslations(choicesArray);
         Log.w("MainActivity.java", "Point 5");
 
         /**translationChoices.get(0).setText(String.valueOf(index.get(number).get(1).get(random)));
@@ -146,6 +184,18 @@ public class MainActivity extends AppCompatActivity {
         translationChoice.setText(String.valueOf(choices.get(0)));
         translationChoice = (TextView) findViewById(R.id.button_2);
         translationChoice.setText(String.valueOf(choices.get(1)));
+        translationChoice = (TextView) findViewById(R.id.button_3);
+        translationChoice.setText(String.valueOf(choices.get(2)));
+        translationChoice = (TextView) findViewById(R.id.button_4);
+        translationChoice.setText(String.valueOf(choices.get(3)));
+        translationChoice = (TextView) findViewById(R.id.button_5);
+        translationChoice.setText(String.valueOf(choices.get(4)));
+        translationChoice = (TextView) findViewById(R.id.button_6);
+        translationChoice.setText(String.valueOf(choices.get(5)));
+        translationChoice = (TextView) findViewById(R.id.button_7);
+        translationChoice.setText(String.valueOf(choices.get(6)));
+        translationChoice = (TextView) findViewById(R.id.button_8);
+        translationChoice.setText(String.valueOf(choices.get(7)));
     }
 
 }
